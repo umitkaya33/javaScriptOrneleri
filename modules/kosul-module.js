e6f1a5324f7723ac2fbddd8d46a883d90ccdbf11
Ümit Kaya <33umitkaya@gmail.com>
2022-11-15 17:46:03 +0300

    if | else if | else örnekleri eklendi

--- modules/kosul-module.js
+++ modules/kosul-module.js
@@ -1,9 +1,22 @@
-var ifFunctionSimple = function() {
-    document.write("<b>IF ELSE Modulü: </b>");
-  };
+var ifFunctionSimple = function () {
+  var time = 24;
+  if (time >= 0 && time <= 5) {
+    document.write("iyi geceler");
+  } else if (time > 5 && time <= 9) {
+    document.write("Günaydın");
+  } else if (time > 9 && time <= 16) {
+    document.write("İyi Günler");
+  } else if (time > 16 && time <= 23) {
+    document.write("İyi Akşamlar");
+  }
+  else if (time > 23) {
+    document.write("Çok Büyük Değer");
+  }
+  else {
+    document.write("Hata");
+  }
+};
 
-
-  
-  export {
-    ifFunctionSimple
-  };
+export {
+  ifFunctionSimple
+};
