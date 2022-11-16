cbaee7c9fb9472d07999a2b357508df76fa5daed
Ümit Kaya <33umitkaya@gmail.com>
2022-11-16 11:08:35 +0300

    Tek bir ifadeyle birden fazla dizi oluşturma

--- modules/diziler-module.js
+++ modules/diziler-module.js
@@ -8,11 +8,18 @@ var arraydefinitonFunctionSimple = function () {
 var arrayExample1 = function () {
     var fruits = new Array("Elma", "Armut", "Kiraz", "Kavun", "Karpuz");
     var show = fruits;
-    document.write(fruits + "<br>")
+    document.write(fruits + "<br>");
     document.write(show[4]);
+};
+var arrayExample2 = function () {
+    var colors = new Array("Siyah", "Kırmızı", "Mavi", "Yeşil", "Turuncu"),
+        persons = new Array("Orkun", "Ümit", "Göknur"),
+        olds = new Array(15, 25, 35, 20);
+    document.write(persons[2] + " " + colors[3] + " " + olds[3]);
 };
 
 export {
     arraydefinitonFunctionSimple,
-    arrayExample1
+    arrayExample1,
+    arrayExample2
 }; 
