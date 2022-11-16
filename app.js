c2c6beb08d255f4cf6a8b5f48abf3d7cd4fd5f17
Ümit Kaya <33umitkaya@gmail.com>
2022-11-16 10:07:32 +0300

    diziler-module eklendi

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-import  * as  degiskenModule from "./modules/degisken-module.js";
-import  * as  dongulerModule from "./modules/donguler-module.js";
-import  * as  kosulModule from "./modules/kosul-module.js";
+import * as  degiskenModule from "./modules/degisken-module.js";
+import * as  dongulerModule from "./modules/donguler-module.js";
+import * as  kosulModule from "./modules/kosul-module.js";
+import * as  dizilerModule from "./modules/diziler-module.js";
+
 
 const btnDongulerWhile = document.getElementById('btnDongulerWhile');
 const btnDegiskenLenght = document.getElementById('btnDegiskenLenght');
-const btnKosulModuleIF= document.getElementById('btnKosulModuleIF');
+const btnKosulModuleIF = document.getElementById('btnKosulModuleIF');
+const btnDiziler = document.getElementById('btnDiziler');
 
- btnDongulerWhile.addEventListener('click',()=>{
-    dongulerModule.dowhileFunctionSimple();
- });
 
-btnDegiskenLenght.addEventListener('click',()=>{
-    degiskenModule.lenghtFunctionSimple();
- });
-
- 
- btnKosulModuleIF.addEventListener('click',()=>{
-    kosulModule.ternaryFunctionSimple();
+btnDongulerWhile.addEventListener('click', () => {
+   dongulerModule.dowhileFunctionSimple();
 });
+
+btnDegiskenLenght.addEventListener('click', () => {
+   degiskenModule.lenghtFunctionSimple();
+});
+
+
+btnKosulModuleIF.addEventListener('click', () => {
+   kosulModule.ternaryFunctionSimple();
+});
+
+btnDiziler.addEventListener('click', () => {
+   dizilerModule.diziFunctionSimple();
+});
+
 
 //  window.onload = function(){
 //     dongulerModule.icIceWhileDongusuOrnegi();
---
+++ modules/diziler-module.js
@@ -0,0 +1,7 @@
+var diziFunctionSimple = function () {
+    document.write("Diziler");
+}
+
+export {
+    diziFunctionSimple
+}; 
