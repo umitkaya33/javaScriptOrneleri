ca7c22382fec932693d059952b5bd091bc9c04ca
Ümit Kaya <33umitkaya@gmail.com>
2022-11-17 21:20:37 +0300

    Çıkartma işlemi

--- modules/islemler-module.js
+++ modules/islemler-module.js
@@ -1,18 +1,26 @@
-//! Javascript'te toplama işlemi
+//! Toplama işlemi
 var toplaFunctionSimple = function () {
     var value1 = 5;
     var value2 = 15;
     var process = value1 + value2;
     document.write(process);
 };
-//! Javascript'te atamalı toplama işlemi
+//! Atamalı toplama işlemi
 var atamalitoplaFunctionSimple = function () {
     var value1 = 22;
     var value2 = 15;
     value1 += value2;
     document.write(value1);
 };
+//! Çıkartma işlemi
+var cikartFunctionSimple = function () {
+    var value1 = 22;
+    var value2 = 44;
+    var process = value1 - value2;
+    document.write(process);
+};
 export {
     toplaFunctionSimple,
-    atamalitoplaFunctionSimple
+    atamalitoplaFunctionSimple,
+    cikartFunctionSimple
 };
