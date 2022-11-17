404e2ab80006d4b5b7f6ce7c9e7f3b4f61006979
Ümit Kaya <33umitkaya@gmail.com>
2022-11-17 21:18:23 +0300

    atamalı toplama işlemi

--- modules/islemler-module.js
+++ modules/islemler-module.js
@@ -1,7 +1,18 @@
 //! Javascript'te toplama işlemi
 var toplaFunctionSimple = function () {
-document.write("Test");
+    var value1 = 5;
+    var value2 = 15;
+    var process = value1 + value2;
+    document.write(process);
+};
+//! Javascript'te atamalı toplama işlemi
+var atamalitoplaFunctionSimple = function () {
+    var value1 = 22;
+    var value2 = 15;
+    value1 += value2;
+    document.write(value1);
 };
 export {
-toplaFunctionSimple
+    toplaFunctionSimple,
+    atamalitoplaFunctionSimple
 };
