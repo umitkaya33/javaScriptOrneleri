737e3abf05a814d4ba4b10acc545f43b2da4f396
Ümit Kaya <33umitkaya@gmail.com>
2022-11-18 10:51:00 +0300

    MIN_VALUE Metodu kullanımı

--- modules/mat-islem-module.js
+++ modules/mat-islem-module.js
@@ -3,6 +3,12 @@ var maxValueFunctionSimple = function () {
     var largestNumber = Number.MAX_VALUE;
     document.write(largestNumber);
 };
+//! MIN_VALUE | Atanabilecek en küçük negatif sayı değeri...
+var minValueFunctionSimple = function () {
+    var smallestNumber = Number.MIN_VALUE;
+    document.write(smallestNumber);
+};
 export {
-    maxValueFunctionSimple
+    maxValueFunctionSimple,
+    minValueFunctionSimple
 };
