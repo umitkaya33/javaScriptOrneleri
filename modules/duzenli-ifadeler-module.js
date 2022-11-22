6d24f5b87efcc9fc5e431f4c1d63a1caaffffa20
Ümit Kaya <33umitkaya@gmail.com>
2022-11-22 20:16:45 +0300

    Düzenli ifadelerde /g ayarı kullanımı

--- modules/duzenli-ifadeler-module.js
+++ modules/duzenli-ifadeler-module.js
@@ -1,11 +1,18 @@
-// match() Kendisine parametre olarak verilen düzenli ifadeli değer doğrultusunda değişken içeriğini kontrol eder ve aranan değerlerin eşleşmesi durumunda eşleşmeyi sağlayan değişken içeriği değerlerinden yeni bir dizi oluşturarak oluşturduğu dizi değerini gösterir...
+//! match() Kendisine parametre olarak verilen düzenli ifadeli değer doğrultusunda değişken içeriğini kontrol eder ve aranan değerlerin eşleşmesi durumunda eşleşmeyi sağlayan değişken içeriği değerlerinden yeni bir dizi oluşturarak oluşturduğu dizi değerini gösterir...
 var matchFunctionSimple = function () {
     var example = "Merhaba Ümit nasılsın?",
         expression = new RegExp("Merhaba"), //? expression = /Ümit/
         result = example.match(expression);
     document.write(result);
-
+};
+//! g |  Düzenli ifade dahilinde kontrol edilecek olan değişken içeriğinin tamamını kontrol edilmesini sağlar...
+var gFunctionSimple = function () {
+    var contents = "Merhaba Ümit nasılsın? Senin adın Ümit miydi?",
+        expression = /Ümit/g,
+        result = contents.match(expression);
+    document.write(result);
 };
 export {
-    matchFunctionSimple
+    matchFunctionSimple,
+    gFunctionSimple
 };
