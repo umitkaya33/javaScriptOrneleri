bada6adad6607beffc5d1141d2422c8f73e2bd40
Ümit Kaya <33umitkaya@gmail.com>
2022-11-22 20:05:17 +0300

    Düzenli ifadelerde match() Metodu kullanımı

--- modules/duzenli-ifadeler-module.js
+++ modules/duzenli-ifadeler-module.js
@@ -1,7 +1,11 @@
-//! Javascriptte Düzenli ifadeler
-var testFunctionSimple = function () {
-    document.write("Düzenli İfadeler");
+// match() Kendisine parametre olarak verilen düzenli ifadeli değer doğrultusunda değişken içeriğini kontrol eder ve aranan değerlerin eşleşmesi durumunda eşleşmeyi sağlayan değişken içeriği değerlerinden yeni bir dizi oluşturarak oluşturduğu dizi değerini gösterir...
+var matchFunctionSimple = function () {
+    var example = "Merhaba Ümit nasılsın?",
+        expression = new RegExp("Merhaba"), //? expression = /Ümit/
+        result = example.match(expression);
+    document.write(result);
+
 };
 export {
-    testFunctionSimple
+    matchFunctionSimple
 };
