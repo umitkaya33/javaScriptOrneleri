36adad1a1240e34b9af022491804ac2187865047
Ümit Kaya <33umitkaya@gmail.com>
2022-11-24 17:58:07 +0300

    Date() yapısının kullanımı

--- modules/tarih-saat-module.js
+++ modules/tarih-saat-module.js
@@ -1,6 +1,11 @@
-var testFunctionSimple = function () {
-    document.write("Tarih ve Saat işlemleri");
+//! Date() | Kullanıldığı değişken içerisine tarih ve saat bilgilerini depolar.(Tarihe etki edilebilir)
+var dateFunctionSimple = function () {
+    var viewTime = new Date(),
+        convert = String(viewTime),
+        partition = convert.split(" ");
+    document.write(partition + "<br>");
+    document.write("YIL: " + partition[3]);
 };
 export {
-    testFunctionSimple
+    dateFunctionSimple
 };
