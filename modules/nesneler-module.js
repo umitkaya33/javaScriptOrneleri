787cf785a1ab47fe9c3fc5f47a1d4f2bcffba88b
Ümit Kaya <33umitkaya@gmail.com>
2022-11-25 20:52:20 +0300

    Nesne tanımlama ve nesneyi çağırma

--- modules/nesneler-module.js
+++ modules/nesneler-module.js
@@ -1,6 +1,13 @@
-var objectsExample = function () {
-  document.write("Test");
+//! Nesne tanımlama ve nesneyi çağırma...
+var objectsExample1 = function () {
+  var info = { userName: "Ümit", userSurname: "Kaya", userDateOfBirth: 1991 },
+    personName = info.userName,
+    personSurname = info.userSurname,
+    personDate = info.userDateOfBirth;
+  document.write("Kişi Adı:" + personName + "<br>");
+  document.write("Kişi Soyadı:" + personSurname + "<br>");
+  document.write("Kişi Doğum Tarihi:" + personDate);
 };
 export {
-objectsExample
+  objectsExample1
 }
