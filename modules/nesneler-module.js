e238947cf2123ae126555553b412b714a0a9443f
Ümit Kaya <33umitkaya@gmail.com>
2022-11-25 21:56:38 +0300

    bir nesneyi başka bir değişkene aktarma

--- modules/nesneler-module.js
+++ modules/nesneler-module.js
@@ -1,28 +1,28 @@
 //! Nesne tanımlama ve nesneyi çağırma...
-var objectsExample1 = function () {
-  var info = { userName: "Ümit", userSurname: "Kaya", userDateOfBirth: 1991 },
-    personName = info.userName,
-    personSurname = info.userSurname,
-    personDate = info.userDateOfBirth;
+function objectsExample1() {
+  var info = { userName: "Ümit", userSurname: "Kaya", userDateOfBirth: 1991 };
+  let personName = info.userName;
+  let personSurname = info.userSurname;
+  let personDate = info.userDateOfBirth;
   document.write("Kişi Adı:" + personName + "<br>");
   document.write("Kişi Soyadı:" + personSurname + "<br>");
   document.write("Kişi Doğum Tarihi:" + personDate);
-};
+}
 //! new operatörü kullanarak nesne tanımlama ve nesneyi çağırma...
-var objectsExample2 = function () {
+function objectsExample2() {
   var info = new Object();
   info.userName = "Ümit";
   info.userSurname = "Kaya";
   info.userDateOfBirth = 1991;
-  var writeName = info.userName,
-    writeSurname = info.userSurname,
-    writeDateOfBirth = info.userDateOfBirth;
+  let writeName = info.userName;
+  let writeSurname = info.userSurname;
+  let writeDateOfBirth = info.userDateOfBirth;
   document.write("Kişi Adı:" + writeName + "<br>");
   document.write("Kişi Soyadı:" + writeSurname + "<br>");
   document.write("Kişi Doğum Tarihi:" + writeDateOfBirth);
-};
+}
 //! fonksiyon şeklinde nesne tanımlama ve nesneyi çağırma...
-var objectsExample3 = function () {
+function objectsExample3() {
   function objectsFunction() {
     this.username = "Ümit";
     this.usersurname = "Kaya";
@@ -30,23 +30,29 @@ var objectsExample3 = function () {
   var info = new objectsFunction();
   document.write("Kişi Adı:" + info.username + "<br>");
   document.write("Kişi Soyadı:" + info.usersurname);
-};
+}
 //! nesnelerde özellik ve değer atamalarında tırnak işaretlerinin kullanımı...
-var objectsExample4 = function () {
+function objectsExample4() {
   let info = { 'username': "Ümit", "usersurname": 'Kaya' };
   document.write(info.username + " " + info.usersurname);
-};
+}
 //! tek bir ifadeyle birden fazla nesne tanımlama...
-var objectsExample5 = function () {
-  var userInfo = { username: "Ümit", usersurname: 'Kaya' },
-    userColor = { color1: "Black", color2: "Red" };
+function objectsExample5() {
+  var userInfo = { username: "Ümit", usersurname: 'Kaya' }, userColor = { color1: "Black", color2: "Red" };
   document.write(userInfo.username + " " + userInfo.usersurname + "<br>");
   document.write(userColor.color1 + " ♥ " + userColor.color2);
-};
+}
+//! bir nesneyi başka bir değişkene aktarma...
+function objectsExample6() {
+  var userInfo = { username: "Ümit", usersurname: 'Kaya' };
+  var transferInfo = userInfo;
+  document.write(transferInfo.username + " " + transferInfo.usersurname + "<br>");
+}
 export {
   objectsExample1,
   objectsExample2,
   objectsExample3,
   objectsExample4,
-  objectsExample5
+  objectsExample5,
+  objectsExample6
 }
