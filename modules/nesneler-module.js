80bf4e04fb3ec2f58dea961048413d8619b690e4
Ümit Kaya <33umitkaya@gmail.com>
2022-11-25 21:03:11 +0300

    new Object() operatörü kullanarak nesne tanımlama ve nesneyi çağırma...

--- modules/nesneler-module.js
+++ modules/nesneler-module.js
@@ -8,6 +8,20 @@ var objectsExample1 = function () {
   document.write("Kişi Soyadı:" + personSurname + "<br>");
   document.write("Kişi Doğum Tarihi:" + personDate);
 };
+//! new operatörü kullanarak nesne tanımlama ve nesneyi çağırma...
+var objectsExample2 = function () {
+  var info = new Object();
+  info.userName = "Ümit";
+  info.userSurname = "Kaya";
+  info.userDateOfBirth = 1991;
+  var writeName = info.userName,
+    writeSurname = info.userSurname,
+    writeDateOfBirth = info.userDateOfBirth;
+  document.write("Kişi Adı:" + writeName + "<br>");
+  document.write("Kişi Soyadı:" + writeSurname + "<br>");
+  document.write("Kişi Doğum Tarihi:" + writeDateOfBirth);
+};
 export {
-  objectsExample1
+  objectsExample1,
+  objectsExample2
 }
